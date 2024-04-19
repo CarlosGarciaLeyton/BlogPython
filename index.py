ba66d69475118089994f325127765eb562f31e47
JCGARCIA <jcarlosgarcial@outlook.com>
2024-04-18 20:28:56 -0400

    Modificacion

--- index.py
+++ index.py
@@ -4,7 +4,6 @@ import requests
 URL = "https://jsonplaceholder.typicode.com/users/1"
 response = requests.get(URL)
 
-
 if response.status_code == 200:
     print('Solicitud exitosa')
     print('Data:', response.json())
