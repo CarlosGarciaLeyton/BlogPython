a061ca85f356c53d5058acf9e4e98aae0201fd79
JCGARCIA <jcarlosgarcial@outlook.com>
2024-04-18 22:23:00 -0400

    url servicio

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 import requests
 
 
-URL = "https://jsonplaceholder.typicode.com/users/1"
+#URL = "https://jsonplaceholder.typicode.com/users/1"
+URL = "https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php"
 responses = requests.get(URL)
 
-if responses.status_code == 400:
+#TODO
+if responses.status_code == 200:
     print('Solicitud exitos')
     print('Data:', responses.json())
+
+
+
 else:
     print('Error en la solicitud, detalle:', responses.text)
