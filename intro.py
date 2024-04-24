42f0605d77e38e2c3a028e0b46a655f04fcefefd
JCGARCIA <jcarlosgarcial@outlook.com>
2024-04-23 22:10:33 -0400

    Feat(add): se agrega archivo con funciones nuevas

---
+++ intro.py
@@ -0,0 +1,45 @@
+#WHILE
+'''count = 51
+while count <= 50:
+    print (f'Hola mundo -> {count}')
+    count = count + 1'''
+  
+
+     
+     
+'''table = int(input ('¿Que tabla quieres ver? : '))  
+count = 1 
+
+while count <= 10:
+     #print (count)
+     #print (f'Z X {count} = ?')
+     #print (f'{table} x {count} = ?')
+     #resultado = table * count10
+     print (f'{table} x {count} = {table * count}')
+     #print (f'{table} x {count} = {resultado}')
+     count  = count + 1 '''
+       
+  
+  
+#FOR
+table = float(input ('¿Que tabla quieres ver?')) 
+nums = range (0,5)
+
+for num in nums:
+    print(num)
+    print (f'{table} - {num} = ?')
+    print (f'{table} - {num} = {table - num}')
+    
+    
+#Clases y objetos
+#1 class Auto:
+   # pass
+   
+'''class Auto:
+    marca = ""
+    color = "blanco"   
+
+auto1 = Auto() 
+print(auto1.color)  ''' 
+    
+  
